5f36b1cf4ef4b262c5fe60e8cf72932ce066ebe9
sandeepsinghmehra <sandysmart017@gmail.com>
2023-08-23 22:20:17 +0530

    Removed export

--- app/api/[storeId]/checkout/pretransaction/route.ts
+++ app/api/[storeId]/checkout/pretransaction/route.ts
@@ -7,7 +7,7 @@ const PaytmChecksum = require("./PaytmChecksum");
 
 connectToDatabase();
 
-export const corsHeaders = {
+const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
