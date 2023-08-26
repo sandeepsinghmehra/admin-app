a41d5cfef0949e39c3145249a3b82ef2c638383e
sandeepsinghmehra <sandysmart017@gmail.com>
2023-08-26 14:51:10 +0530

    Update some field

--- models/OrderItem.ts
+++ models/OrderItem.ts
@@ -1,13 +1,20 @@
 import mongoose, { Document, Schema, Model, model, Types } from "mongoose";
 import { OrderType } from "./Order";
 import { ProductType } from "./Product";
+import { ColorType } from "./Color";
+import { SizeType } from "./Size";
 
 export interface OrderItemType extends Document {
     _id?: Types.ObjectId,
     storeId: string,
     productId: ProductType,
+    name: string,
+    price: string,
     quantity: number,
+    color: ColorType,
+    size: SizeType,
     orderId: OrderType,
+    images: Array<any>,
     createdAt?: Date,
     updatedAt?: Date,
 }
@@ -16,9 +23,22 @@ const orderItemSchema = new Schema<OrderItemType>({
     storeId: String,
     productId: { type: Schema.Types.ObjectId, ref: 'Product' }, // Reference to the Product model
     orderId: { type: Schema.Types.ObjectId, ref: 'Order' }, // Reference to the OrderId model
+    name: String,
     quantity: {
         type: Number
     },
+    price: {
+        type: String
+    },
+    size:  {
+        type: Schema.Types.ObjectId,
+        ref: 'Size', // Referencing the 'Size' model
+    },
+    color:  {
+        type: Schema.Types.ObjectId,
+        ref: 'Color', // Referencing the 'Color' model
+    },
+    images: Array,
     createdAt: {
         type: Date,
         default: Date.now,
